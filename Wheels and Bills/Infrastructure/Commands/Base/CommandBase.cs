bfb5b5c6508362755cb08b8238dd17b7952daf40
DmitryTheCreator <dmitryzamorev@gmail.com>
2022-04-03 18:22:01 +0500

    Базовый класс команды

---
+++ Wheels and Bills/Infrastructure/Commands/Base/CommandBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Wheels_and_Bills.Infrastructure.Commands.Base
+{
+    internal abstract class CommandBase : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value; 
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object parameter) { throw new NotImplementedException(); }
+
+        public void Execute(object parameter) { throw new NotImplementedException(); }       
+    }
+}
