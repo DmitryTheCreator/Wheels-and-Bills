c520bafb81f735f337e9860e8c83444344619f47
DmitryTheCreator <dmitryzamorev@gmail.com>
2022-04-03 18:06:57 +0500

    Класс модели-представления для основного окна

---
+++ Wheels and Bills/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Wheels_and_Bills.ViewModels.Base;
+
+namespace Wheels_and_Bills.ViewModels
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        #region Заголовок окна
+
+        private string _title = "Wheels and Bills";
+
+        /// <summary>
+        /// Заголовок окна
+        /// </summary>       
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+
+        #endregion
+    }
+}
